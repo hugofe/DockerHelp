b660372244e6d26755a057877b257e8ba6963a58
hugofe <jh.ferreirati@gmail.com>
2019-01-10 09:47:57 -0200

    Docker leaning, first commit

---
+++ AulaDocker/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace AulaDocker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            while(true)
+            {
+                Console.WriteLine("Esperando conexão....");
+                Thread.Sleep(10000);
+            }
+        }
+    }
+}
